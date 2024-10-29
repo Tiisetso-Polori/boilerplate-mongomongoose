608c7b8e3d48d564868b7299d67cc910044709d4
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2024-10-29 21:48:44 +0200

    1

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
-require('dotenv').config();
+// Kx8KLiDGb9NFJeIk
+// 41.4.13.98/32
+// tiisetsopolori13
+// 
 
+require('dotenv').config();
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
