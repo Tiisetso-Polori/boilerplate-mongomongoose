01585bc6fc83ed38775c9119c49af1b459d33902
Tyson-Killmoore <118134452+Tiisetso-Polori@users.noreply.github.com>
2024-10-30 12:12:15 +0200

    crud 1

--- myApp.js
+++ myApp.js
@@ -5,10 +5,17 @@
 
 require('dotenv').config();
 let mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
